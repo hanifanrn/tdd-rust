5e496c5f9d5eaa97239549100a0f53643e86ee03
hanifan.nurahman <hanifan.nurahman@ibm.com>
2023-09-23 21:33:21 +0700

    add some test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+//use std::env;
+//use std::io::{self, BufRead, Write};
+//
+//fn simple_array_sum(arr: &[i32]) -> i32 {
+//    let mut ans:i32 = 0;
+//     
+//    for i in 0..arr.len() {
+//        ans += i as i32;
+//    } 
+//     
+//    return ans; 
+//}
+
 fn main() {
     println!("Hello, world!");
 }
+
+
+#[cfg(test)]
+mod test {
+    #[test] 
+    fn sample_unit_test() {
+        assert_eq!(2 + 2, 3);
+    }
+    
+    
+    
+}
+
